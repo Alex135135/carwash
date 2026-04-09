398efe96742ec94bfb67fa80e45ab922a643648b
Alex135135 <jonaton1@yandex.ru>
2026-04-09 23:12:26 +0300

    add change input & fetch

--- src/app/api/send-to-telegram/booking/page.tsx
+++ src/app/api/send-to-telegram/booking/page.tsx
@@ -10,5 +10,43 @@ export default function Page() {
         carModel: '',
         notes: ''
     })
-    const [loading, setloading] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target
+        setFormData(prev => ({ ...prev, [name]: value }))
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
+        try {
+            const res = await fetch('/api/send-to-telegram', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+
+
+            const data = await res.json();
+            if (!res.ok) {
+                console.error('Ошибка от API-роута:', data);
+                throw new Error(data.error || `Status ${res.status}`);
+            }
+
+            console.log('Запись успешно создана:', data);
+            setFormData({ firstName: '', lastName: '', phone: '', carModel: '', notes: '' });
+            alert('Ваша заявка принята! Мы скоро свяжемся с вами.');
+        } catch (err) {
+            const errorMessage = err instanceof Error
+                ? err.message
+                : 'Произошла неизвестная ошибка';
+
+            console.error('Ошибка при создании записи:', err);
+            alert(`Произошла ошибка: ${errorMessage}`);
+        } finally {
+            setLoading(false);
+        }
+    };
+
 }
