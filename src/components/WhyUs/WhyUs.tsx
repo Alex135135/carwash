9c64a2831222d77da0c64310b4a17c39db209b45
Alex135135 <jonaton1@yandex.ru>
2026-04-14 21:45:02 +0300

    added component additionalServices

---
+++ src/components/WhyUs/WhyUs.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export const WhyUs = () => {
+    return (
+        <section>
+            <h2>Почему выбирают нас</h2>
+            {/* Добавьте ваш контент */}
+        </section>
+    );
+};
